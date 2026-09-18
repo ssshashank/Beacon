fe8fd3b721ec9d503f90dd2c2f954ba5fe98e376
ssshashank <shekharshashank97@gmail.com>
2026-09-18 21:55:55 +0530

    Chore:
    - Added Icons

---
+++ apps/web/src/global/components/_icons/memory.tsx
@@ -0,0 +1,29 @@
+import { IconProps } from "../../types";
+
+export const MemoryIcons = ({ size = 24, color = 'currentColor', strokeWidth = 2, ...others }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      stroke-width={strokeWidth}
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      {...others}>
+      <path d="M12 12v-2" />
+      <path d="M12 18v-2" />
+      <path d="M16 12v-2" />
+      <path d="M16 18v-2" />
+      <path d="M2 11h1.5" />
+      <path d="M20 18v-2" />
+      <path d="M20.5 11H22" />
+      <path d="M4 18v-2" />
+      <path d="M8 12v-2" />
+      <path d="M8 18v-2" />
+      <rect x="2" y="6" width="20" height="10" rx="2" />
+    </svg>
+  );
+}
