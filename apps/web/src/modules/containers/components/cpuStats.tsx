54eed980f95efe156fca045bca440be2f66d4662
ssshashank <shekharshashank97@gmail.com>
2026-09-24 17:14:26 +0530

    Fix:

--- apps/web/src/modules/containers/components/cpuStats.tsx
+++ apps/web/src/modules/containers/components/cpuStats.tsx
@@ -1,8 +1,7 @@
 import { createMemo, createSignal, For, onCleanup } from "solid-js";
 import { CPUIcons } from "../../../global/components/_icons/cpu";
+import { BAR_WIDTH, GAP } from "../constant";
 
-const BAR_WIDTH = 3;
-const GAP = 5;
 export function CPUStats(props: any) {
   const [width, setWidth] = createSignal<number>(0);
   let observer: ResizeObserver | undefined;
@@ -17,11 +16,11 @@ export function CPUStats(props: any) {
   onCleanup(() => observer?.disconnect());
   const bufferSize = createMemo(() => Math.max(1, Math.floor((width() + GAP) / (BAR_WIDTH + GAP))));
 
+  // Show the read and write bar
   const displayBars = createMemo(() => {
     const cap = bufferSize();
     const list = props.stats || [];
 
-    console.log(props.stats)
     if (list.length < cap) {
       const pad = new Array(cap - list.length).fill(0);
       return [...pad, ...list.map((item: any) => item?.cpu?.current ?? 0)];
@@ -29,19 +28,21 @@ export function CPUStats(props: any) {
     return list.slice(-cap).map((item: any) => item?.cpu?.current ?? 0);
   });
 
+  // current Disk computation
   const currentDisk = createMemo(() => {
     const list = props.stats;
 
     return list.at(-1)?.cpu?.current ?? 0;
   });
 
-
+  // peak CPU computation
   const peakCPU = createMemo(() => {
     const values = props.stats;
     if (!values.length) return 0;
     return Math.max(...values.map((point: any) => point.cpu.current));
   });
 
+  // avg CPU computation
   const avgCPU = createMemo(() => props.stats.reduce((sum: number, point: any) => sum + point.cpu.current, 0) / props.stats.length);
 
   return (
@@ -58,15 +59,15 @@ export function CPUStats(props: any) {
           </span>
         </div>
       </div>
-      <div class='flex items-center justify-between pt-5'>
-        <div class='px-3 py-0.5  w-fit rounded-md  flex items-center gap-2 bg-gray-500/20'>
+      <div class='flex items-center justify-between pt-5 flex-wrap gap-y-2'>
+        <div class='px-3 py-0.5  w-fit rounded-md  flex items-center gap-2 bg-gray-500/20 flex-wrap'>
           <p class='text-gray-500'>Cores :</p> {props.cpuCores}
         </div>
-        <div class='flex items-center gap-2'>
-          <div class='px-3 py-0.5  w-fit rounded-md  flex items-center gap-2 bg-gray-500/20'>
+        <div class='flex items-center gap-2 flex-wrap'>
+          <div class='px-3 py-0.5  w-fit rounded-md  flex items-center gap-2 bg-gray-500/20 flex-wrap'>
             <p class='text-gray-500'>Avg CPU: </p> {avgCPU()?.toFixed(2)} %
           </div>
-          <div class='px-3 py-0.5  bg-gray-500/20 w-fit rounded-md flex items-center gap-2'>
+          <div class='px-3 py-0.5  bg-gray-500/20 w-fit rounded-md flex items-center gap-2 flex-wrap'>
             <p class='text-gray-500'>Peak CPU:</p> {peakCPU()?.toFixed(2)} %
           </div>
         </div>
@@ -77,23 +78,20 @@ export function CPUStats(props: any) {
         style={{ gap: `${GAP}px` }}>
         <For each={displayBars()}>
           {(val) => {
-            const heightFactor = Math.min(Math.max(val, 0.05), 1);
+            const heightFactor = Math.max(val, 1);
 
             return (
               <div
                 class="bg-[#82f969] origin-bottom transition-all duration-300 rounded-xs shrink-0"
                 style={{
                   width: `${BAR_WIDTH}px`,
-                  height: "100%",
-                  transform: `scaleY(${heightFactor})`,
+                  height: `${heightFactor}px`,
                 }}
               />
             );
           }}
         </For>
       </div>
-
-
     </div>
   );
 }
